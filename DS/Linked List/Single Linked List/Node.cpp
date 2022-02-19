ba9df29f7c72f1000a8553be0abe548d6301a4c6
Rasel Meya <55887770+raselmeya94@users.noreply.github.com>
2022-02-19 11:44:11 +0600

    Create Node.cpp

---
+++ DS/Linked List/Single Linked List/Node.cpp
@@ -0,0 +1,26 @@
+/*
+Author: Md. Rasel Meya
+Dept. of CSTE,
+Noakhali Science & Technology University.
+
+*/
+
+// part:1
+// sigle linked list 
+// create a node 
+
+// using OOP 
+class Node{
+
+    public:
+        int data;
+        Node *next;
+
+};
+
+// using Struct
+struct Node
+{
+    int data;
+    Node *next;
+};
